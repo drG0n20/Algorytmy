f9de9773a6a6f64edd8946becfd536ee9fe5cca4
Br0n50n <marcinster@gmail.com>
2017-07-27 22:47:13 +0200

    commit test dom

---
+++ src/Algorithms/SortowaniePuktow2.java
@@ -0,0 +1,14 @@
+package Algorithms;
+
+
+public class AlgorytmBabelkowyIterator extends AbstractAlgorithm {
+    @Override
+    public String getName() {
+        return "algbabelkowyiterator";
+    }
+
+    @Override
+    public void runAlgorithm(String[] input) {
+
+    }
+}
