e373f5723cc9d67a039613ae91badad5fc79c8e9
Br0n50n <marcinster@gmail.com>
2017-07-31 21:50:24 +0200

    home commit

--- src/Algorithms/Palindrom.java
+++ src/Algorithms/Palindrom.java
@@ -1,29 +1,35 @@
-//package Algorithms;
-//
-//public class Palindrom extends AbstractAlgorithm {
-//    @Override
-//    public String getName() {
-//        return "palindrom";
-//    }
-//
-//    @Override
-//    public void runAlgorithm(String[] input) {
-//
-//        int n = Integer.parseInt(input[1]);
-//
-//    int
-//
-//    private void prepareString() {
-//
-//        String tmp = "";
-//        int len = text.length();
-//        for (int i = 0; i < len; i++) {
-//            if (text.charAt(i) != ' ') {
-//                tmp = tmp + text.charAt(i);
-//            }
-//        }
-//
-//        this.textPrepared = tmp.toLowerCase();
-//    }
-//}
-//}
+package Algorithms;
+
+public class Palindrom extends AbstractAlgorithm {
+    @Override
+    public String getName() {
+        return "palindrom";
+    }
+
+    @Override
+    public void runAlgorithm(String[] input) {
+
+        //array of numbers to be checked
+        String numbers = Integer.parseInt(input[1]);
+        char numbersToChar = numbers.toCharArray();
+
+        //iterate through the numbers
+        for (int i = 0; i < numbersToChar.length; i++) {
+            numbersToChar = numbersToChar[i];
+            int reversedNumber = 0;
+            int temp = 0;
+
+            //reverse the number
+            while (number > 0) {
+                temp = number % 10;
+                number = number / 10;
+                reversedNumber = reversedNumber * 10 + temp;
+            }
+
+            if (numbers[i] == reversedNumber)
+                System.out.println(numbers[i] + " is a palindrome number");
+            else
+                System.out.println(numbers[i] + " is not a palindrome number");
+        }
+    }
+}
